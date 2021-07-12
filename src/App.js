8068add33fe01755db249544289d0e78c39f4044
amdadf <87270315+amdadf@users.noreply.github.com>
2021-07-12 23:37:30 +0600

    two ever

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var persion = {
+    name:"karim",
+    age:"20",
+    job:"singer"
+
+  }
+  var style = {
+    color:"red",
+    backgroundColor:"green",
+    padding:"10px 20px 10px 20px",
+    borderRadius:"80%"
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +24,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      
+        <h2 style={style}>My Name is {persion.name  + "  " + persion.age  + "  " + persion.job  }</h2>
+ 
       </header>
     </div>
   );
